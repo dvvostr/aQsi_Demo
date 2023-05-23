1c2f6184ce11829281b70d9955d8a1c3af09903b
dvvostr <dv_vostr@mail.ru>
2023-05-23 16:39:47 +0300

    Start

--- app/src/main/java/ru/aqsi/aqsidemo/MainActivity.kt
+++ app/src/main/java/ru/aqsi/aqsidemo/MainActivity.kt
@@ -1,13 +1,20 @@
 package ru.aqsi.aqsidemo
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
+import ru.aqsi.commons.models.Cashier
 import ru.aqsi.commons.models.printingModels.FeedPaper
 import ru.aqsi.commons.models.printingModels.QrShtrihCodePrintingModel
+import ru.aqsi.commons.models.printingModels.StringPrintingModel
 import ru.aqsi.commons.receivers.AqsiResultReceiver
-import ru.aqsi.commons.rmk.*
+import ru.aqsi.commons.rmk.ActiveCashierDB
+import ru.aqsi.commons.rmk.AqsiRMK
+import ru.aqsi.commons.rmk.createCashier
+import ru.aqsi.commons.rmk.getAllCashiers
 
 
 class MainActivity : AppCompatActivity() {
@@ -48,17 +55,46 @@ class MainActivity : AppCompatActivity() {
                 }
             }
             +QrShtrihCodePrintingModel(qrString = "https://example.com")
+            +StringPrintingModel("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dictum nibh nisi, quis posuere ex cursus et. Maecenas mattis ullamcorper enim, tempor dignissim magna. Integer eu felis nec dui sagittis egestas eu a mi. Donec nibh orci, scelerisque eget dui in, commodo tincidunt leo. Integer condimentum finibus diam id congue. Aenean vestibulum vitae leo vitae venenatis. Curabitur urna quam, euismod eget lobortis quis, tempor vel tortor. Vestibulum risus lectus, vehicula vel ipsum et, convallis congue quam. Vivamus leo sapien, lacinia a efficitur vel, tristique feugiat felis. Nulla non bibendum eros. Nunc congue elit sapien, tempor laoreet elit feugiat vel. Etiam posuere bibendum justo in hendrerit. Aenean ultricies erat quis nulla iaculis malesuada. Suspendisse a enim mollis, luctus erat nec, vestibulum enim.", 10)
+            +FeedPaper(5)
         }
 
     }
     private fun handleButtonClick2() {
-
+        val activity = this
+        getAllCashiers().forEach {
+            if (it.cashierName == "" && it.cashierPosition ?: "" == "" && it.pin ?: "" == "" ) {
+                if (ActiveCashierDB.setActiveCashier(activity, it.id ?: "")) {
+                    val i = ActiveCashierDB.removeAllActiveCashiers(activity)
+                    Log.d("DROP CASHIER", "Qty=${i}")
+                }
+            }
+        }
+        val list: List<Cashier> = getAllCashiers()
+        list.firstOrNull()?.id?.let {
+            ActiveCashierDB.setActiveCashier(activity, it)
+        }
     }
     private fun handleButtonClick3() {
-
+        val cashier = Cashier(
+            cashierName = "New cashier",
+            cashierPosition = Cashier.ORDINARY,
+            pin = "1234",
+            inn = "001122334455"
+        )
+        val out = createCashier(cashier)
+        Toast.makeText(this, "Create cashier ${if (out)" OK" else "FAILED"}", Toast.LENGTH_LONG).show()
     }
     private fun handleButtonClick4() {
-
+        val activity = this
+        getAllCashiers().forEach {
+            if (it.cashierName == "New cashier" ) {
+                if (ActiveCashierDB.setActiveCashier(activity, it.id ?: "")) {
+                    val out = ActiveCashierDB.removeAllActiveCashiers(activity)
+                    Toast.makeText(this, "Cashier drop ${if (out == 1)" OK" else "FAILED"}", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
     }
     private fun handleButtonClick5() {
 
